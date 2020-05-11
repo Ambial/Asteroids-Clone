15aa1083ec285b35e28a33cbfcdc4eb230acca5d
Simon Körner <simonkoerner@gmx.de>
2020-05-11 16:32:13 +0200

    Chapter-2-9

--- script/main.js
+++ script/main.js
@@ -21,24 +21,19 @@ context.fillText("(50, 50)", 30, 40);
 context.fillText("(150, 250)", 130, 260);
 context.fillText("(250, 170)", 255, 175);
 
-// context.fill();
-
 context.beginPath()
 context.moveTo(50, 250);
-context.lineTo(50, 350);
-context.lineTo(150, 350);
+context.quadraticCurveTo(25, 300, 50, 350);
+context.quadraticCurveTo(100, 375, 150, 350);
 context.closePath();
-
 context.moveTo(230, 360);
-context.lineTo(270, 360);
-context.lineTo(270, 310);
+context.quadraticCurveTo(255, 340, 270, 360);
+context.quadraticCurveTo(255, 340, 270, 310);
 context.closePath();
-
 context.moveTo(250, 50);
-context.lineTo(370, 50);
-context.lineTo(370, 100);
+context.quadraticCurveTo(310, 60, 370, 50);
+context.quadraticCurveTo(400, 75, 370, 100);
 context.closePath();
-
 context.strokeStyle = "#FFFF00";
 context.fillStyle = "#000000";
 context.fill();

09bf46530b7f0308a062b5d55c92778d9cb4cdd3
Simon Körner <simonkoerner@gmx.de>
2020-05-11 16:29:23 +0200

    Chapter-2-6

--- script/main.js
+++ script/main.js
@@ -4,3 +4,42 @@ let canvas = document.getElementById("asteroidz-canvas");
 const context = canvas.getContext("2d");
 
 drawGrid(context, 15, 45, 'red', 'yellow');
+
+context.beginPath();
+context.strokeStyle = "#FFFFFF";
+context.fillStyle = "#00FF00";
+context.lineWidth = 2;
+context.moveTo(50, 50);
+context.lineTo(150, 250);
+context.lineTo(250, 170);
+context.lineTo(320, 280);
+
+context.closePath();
+
+context.stroke();
+context.fillText("(50, 50)", 30, 40);
+context.fillText("(150, 250)", 130, 260);
+context.fillText("(250, 170)", 255, 175);
+
+// context.fill();
+
+context.beginPath()
+context.moveTo(50, 250);
+context.lineTo(50, 350);
+context.lineTo(150, 350);
+context.closePath();
+
+context.moveTo(230, 360);
+context.lineTo(270, 360);
+context.lineTo(270, 310);
+context.closePath();
+
+context.moveTo(250, 50);
+context.lineTo(370, 50);
+context.lineTo(370, 100);
+context.closePath();
+
+context.strokeStyle = "#FFFF00";
+context.fillStyle = "#000000";
+context.fill();
+context.stroke();

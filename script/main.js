b1b0a7de19f8f87d9e8d0e9b90121779e852224e
Simon Körner <simonkoerner@gmx.de>
2020-05-11 22:33:59 +0200

    chapter-3-3-random-pacmen

--- script/main.js
+++ script/main.js
@@ -1,41 +1,20 @@
-import { drawGrid } from './drawing.js'
+import { drawGrid, drawPacman } from './drawing.js'
 
 let canvas = document.getElementById("asteroidz-canvas");
 const context = canvas.getContext("2d");
 
 drawGrid(context, 15, 45, 'red', 'yellow');
 
-context.beginPath();
-context.strokeStyle = "#FFFFFF";
-context.fillStyle = "#00FF00";
-context.lineWidth = 2;
-context.moveTo(50, 50);
-//control point 1 (x,y), control point 2 (x,y), end point (x,y)
-context.bezierCurveTo(0, 0, 80, 250, 150, 250);
-context.bezierCurveTo(250, 250, 250, 250, 250, 170);
-context.bezierCurveTo(250, 50, 400, 350, 320, 280);
+drawPacman(context, { x: 300, y: 200 }, 100, Math.random());
 
-context.closePath();
+const min_radius = 5;
+const max_radius = 50;
+do {
+    let x = context.canvas.width * Math.random();
+    let y = context.canvas.height * Math.random();
+    let radius = min_radius + (max_radius - min_radius) * Math.
+        random();
+    drawPacman(context, { x: x, y: y }, radius, Math.random());
+}
+while (Math.random() < 0.9);
 
-context.stroke();
-context.fillText("(50, 50)", 30, 40);
-context.fillText("(150, 250)", 130, 260);
-context.fillText("(250, 170)", 255, 175);
-
-context.beginPath()
-context.moveTo(50, 250);
-context.quadraticCurveTo(25, 300, 50, 350);
-context.quadraticCurveTo(100, 375, 150, 350);
-context.closePath();
-context.moveTo(230, 360);
-context.quadraticCurveTo(255, 340, 270, 360);
-context.quadraticCurveTo(255, 340, 270, 310);
-context.closePath();
-context.moveTo(250, 50);
-context.quadraticCurveTo(310, 60, 370, 50);
-context.quadraticCurveTo(400, 75, 370, 100);
-context.closePath();
-context.strokeStyle = "#FFFF00";
-context.fillStyle = "#000000";
-context.fill();
-context.stroke();
